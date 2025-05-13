b2861115cfb6120e1c419f7a58dc9000a81d9a0d
Pedro-Philippe <121466793+Pedro-Philippe@users.noreply.github.com>
2025-05-13 00:55:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let botao = document.querySelector("#botao");
+
+botao.addEventListener("click", e=>{
+    document.getElementById("mensagem");mensagem.textContent = "Você é mais forte do que pensa e capaz de ir além do que imagina. Continue, mesmo que pareça difícil, cada passo te leva mais perto do seu objetivo."
+});
